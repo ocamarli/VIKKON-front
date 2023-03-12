f28fb21117247973caa601bea54424130f10d070
ocamarli <108758911+ocamarli@users.noreply.github.com>
2023-03-11 21:48:37 -0600

    again

--- src/pages/Template/AddOptions.jsx
+++ src/pages/Template/AddOptions.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import Modal from '@material-ui/core/Modal';
-import { TextField, Button } from '@material-ui/core';
+import { makeStyles } from '@mui/styles';
+import Modal from '@mui/material/Modal';
+import { TextField, Button } from '@mui/material';
 
 // Estilos personalizados para el modal
 const useStyles = makeStyles((theme) => ({
