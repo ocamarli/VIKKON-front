0174e918cf9fe2568e3a9fd75ad2ce5d531be0ba
Rodrigo Hernandez <electro001_@hotmail.com>
2023-03-19 20:39:25 -0600

    seteando modelo en canvas y HTML

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,16 +1,65 @@
-import * as React from "react"
+import React, { Suspense } from "react";
 import LogoVikkon from "./components/LogoVikon";
-import { Grid } from '@mui/material';
+import { Grid } from "@mui/material";
 import { Canvas } from "react-three-fiber";
 import { Model3d } from "./components/Newmodel";
+import {
+  Environment,
+  useProgress,
+  Html,
+  OrbitControls,
+} from "@react-three/drei";
 
-const SvgComponent = (props) => (
+const SvgComponent = (props) => {
+  function Loader() {
+    const { progress } = useProgress();
+    return <Html center>{progress} %</Html>;
+  }
 
-    <Grid container padding={5} alignItems="center" justifyItems="center"  justifyContent="center">
-      <Grid item xs={10} sx={{filter:"opacity(25%)" }}><LogoVikkon/></Grid>
-      
+  return (
+    <Grid
+      container
+      padding={5}
+      alignItems="center"
+      justifyItems="center"
+      justifyContent="center"
+      sx={{ width: "95vw", height: "95vh" }}
+    >
+      <Canvas>
+        <Html>
+          <Grid
+            item
+            xs={10}
+            sx={{
+              filter: "opacity(25%)",
+              width: "95vw",
+              height: "95vh",
+              position: "absolute",
+              top: "-50vh",
+              left:"-50vw",
+              zIndex: "-1"
+            }}
+          >
+            <LogoVikkon />
+          </Grid>
+        </Html>
+        <Suspense fallback={<Loader />}>
+          <Environment preset="city" />
+          <OrbitControls
+            makeDefault
+            autoRotate
+            autoRotateSpeed={0.3}
+            maxPolarAngle={Math.PI / 2.3}
+            minPolarAngle={Math.PI / 2.3}
+            enableZoom={false}
+            enablePan={false}
+          ></OrbitControls>
+
+          <Model3d />
+        </Suspense>
+      </Canvas>
     </Grid>
+  );
+};
 
-)
-
-export default SvgComponent
+export default SvgComponent;
