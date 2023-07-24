85eb016e45732a2bf1cc38d5eba9fe61ef9ed8df
ocamarli <108758911+ocamarli@users.noreply.github.com>
2023-07-24 17:18:53 -0600

    test

--- src/pages/Template/Components/ListParametersTemplate.jsx
+++ src/pages/Template/Components/ListParametersTemplate.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useMemo, useEffect } from "react";
+import React, { useState, useMemo, useEffect,useCallback } from "react";
 import { Typography, Grid, Paper } from "@mui/material";
 import ItemParameterTemplate from "./ItemParameterTemplate";
 import { getParameters } from "../../../api/axios";
